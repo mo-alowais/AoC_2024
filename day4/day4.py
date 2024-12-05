ec55ac9603dc457bcbd322cd55e0e28c6d52f8b3
Mohamed Alowais <m.alowais24@gmail.com>
2024-12-05 08:09:37 -0500

    day4 part 2

--- day4/day4.py
+++ day4/day4.py
@@ -14,4 +14,14 @@ for r in range(len(arr)):
                 xmasCount += 1                
                 
 print(xmasCount)
+
+xmasCount = 0
 # part 2
+for r in range(1,len(arr)-1):
+    for c in range(1,len(arr[0])-1):
+        if arr[r][c] != "A": continue
+        corners = [arr[r-1][c-1], arr[r-1][c+1], arr[r+1][c-1], arr[r+1][c+1]]
+        if "".join(corners) in ["MMSS", "MSSM", "SSMM", "SMMS"]:
+            xmasCount += 1
+
+print(xmasCount)
