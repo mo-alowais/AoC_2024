82a79aef67495b68c6113e5bc8e28a4b271c473f
Mohamed Alowais <m.alowais24@gmail.com>
2024-12-04 21:08:34 -0500

    part 1 solution

--- day4/day4.py
+++ day4/day4.py
@@ -1,32 +1,17 @@
-input = open('input4.txt','r')
+input = open('input4.txt')
 arr = []
 for line in input:
     arr.append(list(line.strip('\n')))
 
 xmasCount = 0
 # part 1
-
-def isValid(inputRow, idxToCheck : int, rev = False) -> bool:
-    strToCheck = list("XMAS")
-
-    if not strToCheck == arr[idxToCheck:idxToCheck+4] and rev:
-        return False
-    return strToCheck == arr[idxToCheck:idxToCheck+4] or isValid(inputRow[::-1], len(inputRow) - idxToCheck, True) 
-
-# check rows
-for i in range(len(arr)):
-    if arr[i] == 'X'
-    if isValid(arr[i], i):
-        xmasCount += 1
-# check columns
-for i in range(len(arr[0])):
-    if isValid([row[i] for row in arr], i):
-        xmasCount += 1
-# check diagonals
-for i in range(len(arr)):
-    if isValid([arr[j][j] for j in range(len(arr))], i):
-        xmasCount += 1
-    if isValid([arr[j][len(arr) - j - 1] for j in range(len(arr))], i):
-        xmasCount += 1
+for r in range(len(arr)):
+    for c in range(len(arr[0])):
+        if arr[r][c] != "X": continue
+        for dr, dc in [(-1,0),(-1,1),(0,1),(1,1),(1,0),(1,-1),(0,-1),(-1,-1)]:
+            if not(0 <= r + 3 * dr < len(arr) and 0 <= c + 3*dc < len(arr[0])): continue
+            if arr[r + dr][c + dc] == 'M' and arr[r + 2*dr][c + 2*dc] == 'A' and arr[r + 3*dr][c + 3*dc] == 'S':
+                xmasCount += 1                
+                
 print(xmasCount)
 # part 2
